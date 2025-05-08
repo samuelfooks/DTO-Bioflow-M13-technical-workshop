dd4e198288e25b64ecd97cb205e1f08e4f799f26
samuelfooks <samuel.fooks@gmail.com>
2025-05-08 19:26:51 +0200

    watch presentations

--- watch_presentations.sh
+++ watch_presentations.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This script watches for changes in Markdown files and automatically converts them to HTML using Marp CLI.
 echo "Starting watch script..."
 inotifywait -m -r -e close_write --format '%w%f' ./ | while read FILE
 do
